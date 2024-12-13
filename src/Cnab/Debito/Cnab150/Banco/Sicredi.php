ba87b21608fb652f3a6e8fc1862513d11d9285e9
José Alcides <jose.alcides@soluct.com.br>
2024-12-13 15:48:58 -0300

    🐛 Adicionado construct

--- src/Cnab/Debito/Cnab150/Banco/Sicredi.php
+++ src/Cnab/Debito/Cnab150/Banco/Sicredi.php
@@ -2,7 +2,13 @@
 
 namespace Josea\LaravelDebitoEmConta\Cnab\Debito\Cnab150\Banco;
 
-class Sicredi
-{
+use Josea\LaravelDebitoEmConta\Cnab\Debito\Cnab150\AbstractDebito;
+use Josea\LaravelDebitoEmConta\Contracts\Debito\Debito;
 
+class Sicredi extends AbstractDebito implements Debito
+{
+    public function __construct(array $params = [])
+    {
+        parent::__construct($params);
+    }
 }
--- src/Contracts/Debito/Debito.php
+++ src/Contracts/Debito/Debito.php
@@ -4,10 +4,5 @@ namespace Josea\LaravelDebitoEmConta\Contracts\Debito;
 
 interface Debito
 {
-    /**
-     * @return mixed
-     */
-    public function getCpfCnpj();
-    public function getAgencia();
-    public function getIdentificacao();
+
 }
